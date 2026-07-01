a47032388f6cc7af65fc6d0ce4593083600d9ea8
Fabio Silva <109297324+FabioSilva11@users.noreply.github.com>
2026-07-01 03:11:37 -0400

    test: mock MOJI local memory

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:moji_pet/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   testWidgets('MOJI app renders the home screen', (WidgetTester tester) async {
+    SharedPreferences.setMockInitialValues({});
+
     await tester.pumpWidget(const MojiApp());
+    await tester.pump();
 
     expect(find.text('MOJI'), findsOneWidget);
-    expect(find.textContaining('Pet virtual'), findsOneWidget);
+    expect(find.textContaining('Humor:'), findsOneWidget);
   });
 }
