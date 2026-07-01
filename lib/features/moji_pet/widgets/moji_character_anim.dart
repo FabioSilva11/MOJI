a600a850d2a9709b031e747cfd4c88e6a9b4ca1b
Fabio Silva <109297324+FabioSilva11@users.noreply.github.com>
2026-07-01 03:08:46 -0400

    feat: add MOJI animation wrapper

---
+++ lib/features/moji_pet/widgets/moji_character_anim.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../domain/moji_animation.dart';
+import '../domain/moji_mood.dart';
+import 'moji_character.dart' as old;
+
+class MojiCharacterAnim extends StatelessWidget {
+  const MojiCharacterAnim({required this.mood, required this.animation, super.key});
+
+  final MojiMood mood;
+  final MojiAnimation animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: animation.profile.duration,
+      child: old.MojiCharacter(
+        key: ValueKey('${mood.name}-${animation.name}'),
+        mood: mood == MojiMood.dizzy ? MojiMood.excited : mood,
+      ),
+    );
+  }
+}
