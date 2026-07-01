1ebcd162716bdf3f6ce69aa1852812fbd5ef5e82
Fabio Silva <109297324+FabioSilva11@users.noreply.github.com>
2026-07-01 03:36:53 -0400

    feat: connect EMO-style API actions to MOJI brain

--- lib/features/moji_pet/domain/moji_brain.dart
+++ lib/features/moji_pet/domain/moji_brain.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 
 import '../../../services/moji_memory_service.dart';
+import 'moji_action.dart';
 import 'moji_animation.dart';
 import 'moji_dialog_planner.dart';
 import 'moji_lines.dart';
@@ -14,6 +15,7 @@ class MojiBrain extends ChangeNotifier {
   MojiMood mood = MojiMood.curious;
   MojiAnimation animation = MojiAnimation.bootup3;
   String message = MojiLines.any(MojiLines.boot);
+  MojiAction? lastAction;
 
   int hunger = 28;
   int energy = 82;
@@ -80,6 +82,71 @@ class MojiBrain extends ChangeNotifier {
     notifyListeners();
   }
 
+  void runAction(MojiAction action) {
+    final profile = action.profile;
+    lastAction = action;
+    _setMood(profile.mood, customMessage: profile.message, nextAnimation: profile.animation);
+
+    switch (action) {
+      case MojiAction.petted:
+        affection += 8;
+        fun += 3;
+        _gainXp(4);
+        break;
+      case MojiAction.shaking:
+      case MojiAction.falling:
+        energy -= 6;
+        fun -= 2;
+        break;
+      case MojiAction.charging:
+        energy += 18;
+        break;
+      case MojiAction.lowPower:
+        energy -= 4;
+        break;
+      case MojiAction.playingGame:
+      case MojiAction.dancing:
+        fun += 18;
+        energy -= 10;
+        hunger += 5;
+        _gainXp(10);
+        break;
+      case MojiAction.sleeping:
+        energy += 20;
+        fun -= 2;
+        break;
+      case MojiAction.wakeup:
+        energy += 5;
+        break;
+      case MojiAction.listening:
+      case MojiAction.speaking:
+      case MojiAction.answeringSmalltalk:
+      case MojiAction.answeringUtility:
+      case MojiAction.answeringQuestion:
+        affection += 2;
+        _gainXp(3);
+        break;
+      case MojiAction.exploring:
+      case MojiAction.searching:
+      case MojiAction.moving:
+        energy -= 6;
+        fun += 4;
+        break;
+      case MojiAction.lifted:
+      case MojiAction.obstacleDetected:
+      case MojiAction.soundDetected:
+      case MojiAction.playingMusic:
+      case MojiAction.lookingAtFace:
+      case MojiAction.seesNonhuman:
+      case MojiAction.staying:
+      case MojiAction.lookingAround:
+        break;
+    }
+
+    _normalize();
+    notifyListeners();
+  }
+
   void tick() {
     _idleTicks++;
     hunger += 2;
@@ -88,15 +155,13 @@ class MojiBrain extends ChangeNotifier {
     affection -= _idleTicks % 6 == 0 ? 1 : 0;
 
     if (_idleTicks % 5 == 0) {
-      _setMood(MojiMood.neutral, customMessage: MojiLines.any(MojiLines.idle), nextAnimation: MojiAnimation.blink2);
-    } else if (_idleTicks % 3 == 0 && mood == MojiMood.neutral) {
-      _setMood(_random.nextBool() ? MojiMood.curious : MojiMood.neutral, nextAnimation: MojiAnimation.neutral);
+      runAction(MojiAction.lookingAround);
     }
 
     if (hunger > 82) {
       _setMood(MojiMood.sad, customMessage: MojiLines.any(MojiLines.hungry), nextAnimation: MojiAnimation.sad);
     } else if (energy < 18) {
-      _setMood(MojiMood.sleepy, customMessage: MojiLines.any(MojiLines.sleep), nextAnimation: MojiAnimation.blink);
+      runAction(MojiAction.lowPower);
     } else if (fun < 18) {
       _setMood(MojiMood.bored, customMessage: MojiLines.any(MojiLines.bored), nextAnimation: MojiAnimation.sad);
     }
@@ -105,40 +170,16 @@ class MojiBrain extends ChangeNotifier {
     notifyListeners();
   }
 
-  void petHead() {
-    _touchCombo++;
-    affection += 8;
-    fun += 4;
-    energy -= 1;
-    _gainXp(4);
-
-    if (_touchCombo >= 5) {
-      _touchCombo = 0;
-      _setMood(MojiMood.angry, customMessage: MojiLines.any(MojiLines.angry), nextAnimation: MojiAnimation.angry);
-    } else if (affection > 82) {
-      _setMood(MojiMood.love, customMessage: MojiLines.any(MojiLines.love), nextAnimation: MojiAnimation.happy3);
-    } else {
-      final happyAnimations = <MojiAnimation>[MojiAnimation.happy, MojiAnimation.happy2, MojiAnimation.happy3];
-      _setMood(
-        MojiMood.happy,
-        customMessage: MojiLines.any(MojiLines.happy),
-        nextAnimation: happyAnimations[_random.nextInt(happyAnimations.length)],
-      );
-    }
-
-    _normalize();
-    notifyListeners();
-  }
+  void petHead() => runAction(MojiAction.petted);
 
   void poke() {
-    fun += 3;
-    energy -= 2;
-    final moods = <MojiMood>[MojiMood.curious, MojiMood.angry, MojiMood.excited];
-    final selected = moods[_random.nextInt(moods.length)];
-    _setMood(selected, customMessage: MojiLines.any(MojiLines.poke));
-    _gainXp(2);
-    _normalize();
-    notifyListeners();
+    _touchCombo++;
+    if (_touchCombo >= 5) {
+      _touchCombo = 0;
+      runAction(MojiAction.shaking);
+    } else {
+      runAction(MojiAction.soundDetected);
+    }
   }
 
   void feed() {
@@ -151,32 +192,12 @@ class MojiBrain extends ChangeNotifier {
     notifyListeners();
   }
 
-  void play() {
-    fun += 30;
-    energy -= 16;
-    hunger += 8;
-    affection += 7;
-    _setMood(
-      energy > 18 ? MojiMood.excited : MojiMood.sleepy,
-      customMessage: energy > 18 ? MojiLines.any(MojiLines.play) : MojiLines.any(MojiLines.sleep),
-      nextAnimation: energy > 18 ? MojiAnimation.excited : MojiAnimation.blink,
-    );
-    _gainXp(10);
-    _normalize();
-    notifyListeners();
-  }
+  void play() => runAction(MojiAction.playingGame);
 
-  void sleep() {
-    energy += 34;
-    hunger += 3;
-    fun -= 2;
-    _setMood(MojiMood.sleepy, customMessage: MojiLines.any(MojiLines.sleep), nextAnimation: MojiAnimation.blink);
-    _gainXp(3);
-    _normalize();
-    notifyListeners();
-  }
+  void sleep() => runAction(MojiAction.sleeping);
 
   void talk() {
+    runAction(MojiAction.listening);
     final line = MojiDialogPlanner.think(
       MojiDialogContext(
         mood: mood,
@@ -195,25 +216,11 @@ class MojiBrain extends ChangeNotifier {
   }
 
   void shakeReaction() {
-    final roll = _random.nextInt(4);
-    if (roll == 0) {
-      _setMood(MojiMood.excited, customMessage: MojiLines.any(MojiLines.dizzy), nextAnimation: MojiAnimation.dizzy);
-    } else {
-      final moods = <MojiMood>[MojiMood.angry, MojiMood.sad, MojiMood.excited];
-      final reaction = moods[_random.nextInt(moods.length)];
-      _setMood(reaction, customMessage: MojiLines.any(MojiLines.impact));
-    }
-    energy -= 5;
-    fun += 4;
-    _normalize();
-    notifyListeners();
+    final actions = <MojiAction>[MojiAction.shaking, MojiAction.falling, MojiAction.lifted];
+    runAction(actions[_random.nextInt(actions.length)]);
   }
 
-  void calmDown() {
-    _touchCombo = 0;
-    _setMood(MojiMood.neutral, customMessage: MojiLines.any(MojiLines.idle), nextAnimation: MojiAnimation.neutral);
-    notifyListeners();
-  }
+  void calmDown() => runAction(MojiAction.staying);
 
   double get hungerValue => hunger / 100;
   double get energyValue => energy / 100;
